e14529905a42bbdf1e4312c4909baf932b4aca54
Kacper Martela <kacper99@hotmail.co.uk>
2018-03-11 23:21:03 +0000

    Created constructors and added remaining methods

--- src/CircularArrayRing.java
+++ src/CircularArrayRing.java
@@ -3,18 +3,28 @@ import java.util.Iterator;
 
 public class CircularArrayRing<E> extends AbstractCollection<E> implements Ring<E> {
 
-    private int ringSize;
+    private int ringSize, head, tail, elementCount;
 
     public CircularArrayRing() {
         this.ringSize = 10;
+        head = tail = elementCount = 0;
     }
 
     public CircularArrayRing(int ringSize) {
         this.ringSize = ringSize;
+        head = tail = elementCount = 0;
+    }
+
+    @Override
+    public boolean add(E e) {
+        return super.add(e);
     }
 
     @Override
     public E get(int index) throws IndexOutOfBoundsException {
+        if (index > ringSize || index > elementCount)
+            throw new IndexOutOfBoundsException("Index out of bounds");
+
         return null;
     }
 

70b4ab46f01f6856c23f2342eff670d000aeb508
Kacper Martela <kacper99@hotmail.co.uk>
2018-03-13 16:20:07 +0000

    Added comments

--- src/CircularArrayRing.java
+++ src/CircularArrayRing.java
@@ -5,42 +5,67 @@ import java.util.NoSuchElementException;
 
 public class CircularArrayRing<E> extends AbstractCollection<E> implements Ring<E> {
 
-    private static final int DEFAULT_RING_SIZE = 10;
+    private static final int DEFAULT_RING_SIZE = 10; //Default size for the ring
     private int head, elementCount;
-    private Object[] ringArray;
+    private Object[] ringArray; //Used to store the objects.
 
+    /**
+     * This constructor sets the ring size to the default size
+     */
     public CircularArrayRing() {
         this.ringArray = new Object[DEFAULT_RING_SIZE];
-        head = elementCount = 0;
+        elementCount = 0;
+        head = -1;
     }
 
+    /**
+     * Allows for a custom ring size
+     * @param ringSize number of elements to have in the ring
+     */
     public CircularArrayRing(int ringSize) {
         this.ringArray = new Object[ringSize];
-        head = elementCount = 0;
+        elementCount = 0;
+        head = -1;
     }
 
+    /**
+     * Adds an element to the ring
+     * @param e the element ot add
+     * @return whether the element has been added or not
+     */
     @Override
     public boolean add(E e) {
-        head++;
+        head++; //Increment the head by one
         if (head == ringArray.length)
-            head = 0;
-        ringArray[head] = e;
-        if (elementCount < ringArray.length)
+            head = 0; //If we get to the end of the ring set the pointer to be 0 again to loop back round
+        ringArray[head] = e; //Get the element
+        if (elementCount < ringArray.length) //Increase the element count up until the length because at that point the number of elements cant change.
             elementCount++;
         return true;
     }
 
+    /**
+     * Gets an item from the ring
+     * @param index The index of the ring to get
+     * @return The item corresponding to that index
+     * @throws IndexOutOfBoundsException Thrown if index is out of bounds
+     */
     @Override
     public E get(int index) throws IndexOutOfBoundsException {
 
         System.out.println(Arrays.toString(ringArray));
-        if (index >= ringArray.length || index < 0)
+        if (index >= ringArray.length || index < 0) //Check if the index is within the bounds of the ring.
             throw new IndexOutOfBoundsException("Index out of bounds");
 
-        int newIndex = head - index;
-        return (E) (newIndex < 0 ? ringArray[newIndex + ringArray.length] : ringArray[newIndex]);
+        int newIndex = head - index; //Adjust the index for the head position
+        return (E) (newIndex < 0 ? ringArray[newIndex + ringArray.length] : ringArray[newIndex]); //If the newIndex is less than zero then add the length of the array to the index
     }
 
+    /**
+     * Creates an iterator for the ring. Iterates backwards around the ring.
+     *
+     * @return The iterator
+     */
     @Override
     public Iterator<E> iterator() {
 
@@ -50,7 +75,7 @@ public class CircularArrayRing<E> extends AbstractCollection<E> implements Ring<
 
             @Override
             public boolean hasNext() {
-                if (currIndex < ringArray.length && ringArray[currIndex] != null) {
+                if (currIndex < ringArray.length && ringArray[currIndex] != null) { //Checking if we got to the end and if there are no null elements
                     return false;
                 }
                 return true;
@@ -58,11 +83,11 @@ public class CircularArrayRing<E> extends AbstractCollection<E> implements Ring<
 
             @Override
             public E next() {
-                Object nextElement = ringArray[currIndex];
-                if (!hasNext())
+                if (!hasNext()) //If there isnt another element then throw an exception
                     throw new NoSuchElementException("No element");
-                currIndex--;
-                if (currIndex < 0)
+                Object nextElement = ringArray[currIndex]; //Getting the next element
+                currIndex--; //Decrease the current index by one
+                if (currIndex < 0) //If we get to the beginning of the
                     currIndex = ringArray.length - 1;
                 return (E) nextElement;
             }
